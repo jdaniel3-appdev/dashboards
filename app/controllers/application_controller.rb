e5936ddad5f434992e8ac3bcdb67adb4f7d76c34
RSCosworth4x4 <jdaniel3@chicagobooth.edu>
2021-02-07 06:34:29 +0000

    Commit 1 FX pages all done, rails grade 100%; didn't do the COVID part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,40 @@
 class ApplicationController < ActionController::Base
+
+  def render_dashboards
+    render({:template => "landing_templates/dashboards.html.erb"})    
+  end
+
+  def list_base_currencies
+    url_ccy_symbol_data = "https://api.exchangerate.host/symbols"
+    raw_symbol_data = open(url_ccy_symbol_data).read
+    parsed_symbol_data = JSON.parse(raw_symbol_data)
+    @currency_symbols = parsed_symbol_data.fetch("symbols").keys  
+
+    render({:template => "landing_templates/list_base_currencies.html.erb"})
+    
+  end
+
+  def list_price_currencies
+    url_ccy_symbol_data = "https://api.exchangerate.host/symbols"
+    raw_symbol_data = open(url_ccy_symbol_data).read
+    parsed_symbol_data = JSON.parse(raw_symbol_data)   
+    @price_currencies = parsed_symbol_data.fetch("symbols").keys
+
+    render({:template => "landing_templates/list_price_currencies.html.erb"})
+    
+  end
+
+
+  def render_fx_page
+    @base_ccy = params.fetch("base").upcase
+    @price_ccy = params.fetch("price").upcase  
+    api_link_text = "https://api.exchangerate.host/convert?from=" + @base_ccy + "&to=" + @price_ccy
+    raw_fx_rate_data = open(api_link_text).read
+    parsed_fx_rate_data = JSON.parse(raw_fx_rate_data)
+    @fx_price_per_base = parsed_fx_rate_data.fetch("info").fetch("rate")
+
+    render({:template => "page_templates/fx_page.html.erb"})
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", {:controller => "application", :action => "render_dashboards"})
+  get("/forex", {:controller => "application", :action => "list_base_currencies"})
+
+  get("/forex/:base", {:controller => "application", :action => "list_price_currencies"})
+  get("/forex/:base/:price", {:controller => "application", :action => "render_fx_page"})
+
 end
